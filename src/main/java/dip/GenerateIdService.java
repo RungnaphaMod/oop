0405e68386cf44922ebc634d09d4fcc5447f3cc1
Moodang Rungnapha <rungnapha.kon@ngernturbo.com>
2021-07-24 12:29:12 +0700

    fourth commit

---
+++ src/main/java/dip/GenerateIdService.java
@@ -0,0 +1,17 @@
+
+package dip;
+
+import java.util.Random;
+
+public class GenerateIdService {
+    private Random random;
+
+    public void setRandom(Random random) {
+        this.random = random;
+    }
+
+    public String getId() {
+        int number = random.nextInt(10);
+        return  "XYZ" + number;
+    }
+}
